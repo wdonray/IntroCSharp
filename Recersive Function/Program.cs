7a81a542bcfc81811e72bde150d72b721c9c1d48
Donray Williams <w.donray@yahoo.com>
2017-01-06 10:22:57 -0600

    Fibonacci

---
+++ Recersive Function/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recersive_Function
+{
+
+    class Program
+    {
+        static int Fibonacci(int a)
+        {
+            if (a <= 1)
+                return 1;
+            return Fibonacci(a - 1) + Fibonacci(a - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+            Console.Read();
+        }
+    }
+}
